d4329df4fb31f702b3df1035a50c4f649019cde5
chandrugtx8 <85623777+chandrugtx8@users.noreply.github.com>
2024-04-05 11:16:05 +0530

    Update Streamlitapi.py

--- Streamlitapi.py
+++ Streamlitapi.py
@@ -1,9 +1,19 @@
-import pickle
 import streamlit as st
 import pandas as pd
+import numpy as np
+import pickle
+import requests
+from io import BytesIO
+
+# Function to load data from GitHub
+def load_data(url):
+    response = requests.get(url)
+    data = response.content
+    return pd.read_csv(BytesIO(data))
 
 # Load the features (X)
-df = pd.read_csv('Flat prices.csv')
+df_url = 'https://raw.githubusercontent.com/chandrugtx8/housing-sales/main/Flat%20prices.csv'
+df = load_data(df_url)
 df['month'] = pd.to_datetime(df['month'])
 df['year'] = df['month'].dt.year
 df['month_of_year'] = df['month'].dt.month
@@ -11,23 +21,19 @@ df['remaining_lease_years'] = df['remaining_lease'].apply(lambda x: int(x.split(
 df.drop(columns=['month', 'remaining_lease', 'block', 'street_name'], inplace=True)
 X = pd.get_dummies(df, columns=['town', 'flat_type', 'storey_range', 'flat_model']).drop(columns=['resale_price'])
 
+# Load the trained model
+model_url = 'https://github.com/chandrugtx8/housing-sales/raw/main/random_forest_model2%20(1).pkl'
+response = requests.get(model_url)
+model = pickle.load(BytesIO(response.content))
+
 def main(X):
     st.title('Housing Sales Prediction')
 
-    town_options = df['town'].unique()
-    town = st.selectbox("Select the town:", town_options)
-
-    flat_type_options = df['flat_type'].unique()
-    flat_type = st.selectbox("Select the flat type:", flat_type_options)
-
-    storey_range_options = df['storey_range'].unique()
-    storey_range = st.selectbox("Select the storey range:", storey_range_options)
-
+    town = st.text_input("Enter the town: ")
+    flat_type = st.text_input("Enter the flat type: ")
+    storey_range = st.text_input("Enter the storey range: ")
     floor_area_sqm_str = st.text_input("Enter the floor area (in sqm): ")
-    
-    flat_model_options = df['flat_model'].unique()
-    flat_model = st.selectbox("Select the flat model:", flat_model_options)
-
+    flat_model = st.text_input("Enter the flat model: ")
     lease_commence_date_str = st.text_input("Enter the lease commence date: ")
 
     if st.button('Predict'):
@@ -43,14 +49,7 @@ def main(X):
                 'lease_commence_date': [lease_commence_date]
             })
 
-            # Ensure input data is encoded in the same way as training data
             input_data_encoded = pd.get_dummies(input_data).reindex(columns=X.columns, fill_value=0)
-
-            # Load the trained model
-            with open('random_forest_model2 (1).pkl', 'rb') as file:
-                model = pickle.load(file)
-
-            # Make prediction
             predicted_price = model.predict(input_data_encoded)[0]
             st.success('Predicted Resale Price: {:.2f}'.format(predicted_price))
         except ValueError:
@@ -58,5 +57,5 @@ def main(X):
 
 if __name__ == '__main__':
     main(X)
- 
+
 
